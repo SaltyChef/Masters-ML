f6ec228ca1800b98a6a6d5631a893d97b770ea6d
TutsZaza <anarco.lapis@gmail.com>
2023-09-25 15:21:10 +0100

    Tabela

--- PL/Aula_Pratica1_APC_Arthur_Rosario.m
+++ PL/Aula_Pratica1_APC_Arthur_Rosario.m
@@ -1,7 +1,8 @@
 %% Calculo de entropia por tabela.
+% possibilidades=input("possibilidades: ");
 positivos=input("Entre os valores positivos encontrados na tabela: "); %valores de entropia positivo
 negativos=input("Entre os valores negativos encontrados na tabela: ");%valores de entropia negativo
 total=positivos+negativos; %soma dos valores anteriores
 entropia=@(pos, neg,total) -pos/(total)*log2(pos/(total))-neg/total*log2(neg/total); %funcao de entropia
-resultado=entropia(positivos,negativos,total);
+resultado=entropia(positivos,negativos,total)
 %%
