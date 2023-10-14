b7a7082fa6635aabee6f46f8055531de756369fa
Diogo Rosário <41454360+SaltyChef@users.noreply.github.com>
2023-10-14 17:27:34 +0100

    models update

--- PL/assignment_1/myclassify.m
+++ PL/assignment_1/myclassify.m
@@ -1,26 +1,26 @@
 function res = myclassify(P) 
     %load net.mat;
 
-    % --------- -----------                    %       epochs
-    %load netLinear.mat;                     %66      10000 
-    %load netLinear_trainscg.mat             %62      10000
-    %load netLinear_traingdm.mat             %14      10000
-    %load netLinear_trainlm.mat              %58      10000
-    %load netLinear_traingda.mat             %28      10000
-    %load netLinear_traingd.mat              %66      50000 
-    %load netLinear_traingd_2L.mat           %50      10000
+    % --------- -----------                         %       epochs
+    %load models/netLinear.mat;                     %66      10000 
+    %load models/netLinear_trainscg.mat             %62      10000
+    %load models/netLinear_traingdm.mat             %14      10000
+    %load models/netLinear_trainlm.mat              %58      10000
+    %load models/netLinear_traingda.mat             %28      10000
+    %load models/netLinear_traingd.mat              %66      50000 
+    %load models/netLinear_traingd_2L.mat           %50      10000
     % ------------------------
-    %load netBinary_traingd.mat              %18      10000
-    %load netBinary_traingda.mat             %22      10000
-    %load netBinary_trainscg.mat             %12      10000
-    %load netBinary_traingdm.mat             %8       10000
-    %load netBinary_trainlm.mat              %10      10000
+    %load models/netBinary_traingd.mat              %18      10000
+    %load models/netBinary_traingda.mat             %22      10000
+    %load models/netBinary_trainscg.mat             %12      10000
+    %load models/netBinary_traingdm.mat             %8       10000
+    %load models/netBinary_trainlm.mat              %10      10000
     %-------------------------
-    %load netSigmoidal_traingd.mat           %36      10000
-    %load netSigmoidal_traingda.mat          %42      10000
-    %load netSigmoidal_trainscg.mat          %70      10000
-    %load netSigmoidal_traingdm.mat          %36      10000
-    load netSigmoidal_trainlm.mat           %56      10000
+    %load models/netSigmoidal_traingd.mat           %36      10000
+    %load models/netSigmoidal_traingda.mat          %42      10000
+    %load models/netSigmoidal_trainscg.mat          %70      10000
+    %load models/netSigmoidal_traingdm.mat          %36      10000
+    load models/netSigmoidal_trainlm.mat           %56      10000
 
     view(net); %View net
     f=readmatrix("printTest.txt");
