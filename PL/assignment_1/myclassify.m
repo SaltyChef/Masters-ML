f268a7bf7dbf669cb3789ad1837a3d3133d95af8
Diogo Rosário <41454360+SaltyChef@users.noreply.github.com>
2023-10-16 22:18:11 +0100

    new classify

--- PL/assignment_1/myclassify.m
+++ PL/assignment_1/myclassify.m
@@ -1,29 +1,15 @@
 function res = myclassify(P, modelName, hasFilter, filterName) 
-    
-    % --------- -----------                         %       epochs
-    %load models/netLinear_traingd.mat              %66     500000
-    %load models/netLinear_trainscg.mat             %62      10000
-    %load models/netLinear_traingdm.mat             %14      10000
-    %load models/netLinear_trainlm.mat              %58      10000
-    %load models/netLinear_traingda.mat             %28      10000
-    %--load models/netLinear_traingd_2L.mat           %50      10000
-    % ------------------------
-    %load models/netBinary_traingd.mat              %18      10000
-    %load models/netBinary_traingda.mat             %22      10000
-    %load models/netBinary_trainscg.mat             %12      10000
-    %load models/netBinary_traingdm.mat             %8       10000
-    %load models/netBinary_trainlm.mat              %10      10000
-    %-------------------------
-    %load models/netSigmoidal_traingd.mat           %36      10000
-    %load models/netSigmoidal_traingda.mat          %42      10000
-    %load models/netSigmoidal_trainscg.mat          %70      10000
-    %load models/netSigmoidal_traingdm.mat          %36      10000
-    %load models/netSigmoidal_trainlm.mat           %56      10000
+    %                                               %       ephocs
+    %models/netLinear_1L.mat                        10%     10000
+    %models/netLinear_2L.mat                        72%     10000
+    %models/netHardlim_1L.mat                       60%     10000
+    %models/nethardlim_2L.mat ???????????????????????????????????
+    %models/netSigmoid_1L.mat                       12%     10000
+    %models/netSigmoid_2L.mat                       58%     10000
     %----------- Filters ----------
     %load models/PerceptronFilter.mat                
-    %load models/TESTE.mat                          %22      10000
+    %load models/AssociativeMemoryFilter.mat              
     
-    %load model
     filename = "models/"+ modelName + ".mat";
     file = load(filename, "net");
     net = file.net;
