d0f8f5195bfc204b802078cae681aa4bdbd56e59
TutsZaza <anarco.lapis@gmail.com>
2023-11-05 15:42:31 +0000

    a

--- PL/assignment_2/correctTarget.m
+++ PL/assignment_2/correctTarget.m
@@ -3,7 +3,7 @@ result = [ones(length(T),1) zeros(length(T),1) zeros(length(T),1)];
 controle = [];
 valido=1;
 for i=1:length(T)
-    if(T(i)==1 & valido)
+    if(T(i)==1 && valido)
         controle=[controle i];
         valido=0;
     else if(T(i)==0 & ~valido)
