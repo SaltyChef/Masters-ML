00fd71ae99b311a0273881bc11e028b98f7237af
TutsZaza <anarco.lapis@gmail.com>
2023-11-15 20:34:59 +0000

    aa

--- PL/assignment_2/utils/LSTM.m
+++ PL/assignment_2/utils/LSTM.m
@@ -1,8 +1,9 @@
 function resultado = LSTM(P,T)
 
     XTrain=P;
-    YTrain=correctTarget(T);
-    
+    YTrain=correctTarget2(T);
+    % YTrain=T;
+
     percentage = 0.85;
     
     breakingIndex = round(length(XTrain) * percentage);
@@ -12,9 +13,10 @@ function resultado = LSTM(P,T)
     target_treino = YTrain(1:breakingIndex, :);
     target_test = YTrain(breakingIndex+1:end , :);
     
-    edges=min(min(data_treino)):0.1:max(max(data_treino));
-    data_treino=discretize(data_treino,edges);
-    data_treino=categorical(data_treino);
+    data_treino=num2cell(data_treino',1);
+    % edges=min(min(data_treino)):0.1:max(max(data_treino));
+    % data_treino=discretize(data_treino,edges);
+    % data_treino=categorical(data_treino);
 
     target_treino=categorical(target_treino);
 
@@ -28,13 +30,15 @@ function resultado = LSTM(P,T)
         fullyConnectedLayer(numClasses)
         softmaxLayer
         classificationLayer
+
     ];
     options=trainingOptions('adam', ...
-        'MaxEpochs',250,...
+        'MaxEpochs',80,...
         'GradientThreshold',1,...
         'InitialLearnRate',0.005,...
         'LearnRateDropFactor',0.2, ...
         'Verbose',0, ...
         'Plots','training-progress');
     net=trainNetwork(data_treino',target_treino',layers,options);
+    save("LSTM.mat","net");
 end
